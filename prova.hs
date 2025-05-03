948724dd0fe3118052b0411654f021b7f9d50872
Davi Alves Rodrigues <144604229+davialves1820@users.noreply.github.com>
2025-05-03 10:33:00 -0300

    Update prova.hs

--- prova.hs
+++ prova.hs
@@ -1,5 +1,6 @@
 import Data.Char (toUpper)
 
+-- Função para compactar lista
 prova :: [Char] -> [(Int, Char)]
 prova [] = []
 prova (x:xs) = (length (x : takeWhile (==x) xs), toUpper x) : prova (dropWhile (==x) xs)
