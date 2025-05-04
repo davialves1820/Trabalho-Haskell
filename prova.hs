45f559948caca7724ad3993003030ba5e756b61c
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-05-04 11:16:02 -0300

    Update prova.hs

--- prova.hs
+++ prova.hs
@@ -1,11 +1,14 @@
+-- Importação da função para transformar um caractere minúsculo em maiúsculo
 import Data.Char (toUpper)
 
 -- Função para compactar lista
-compactar_lista :: [Char] -> [(Int, Char)]
+-- Compacta uma lista de caracteres consecutivos iguais em pares do tipo (quantidade, caractere)
+compactar_lista :: [Char] -> [(Int, Char)] 
 compactar_lista [] = []
 compactar_lista (x:xs) = (length (x : takeWhile (==x) xs), toUpper x) : compactar_lista (dropWhile (==x) xs)
 
 -- Função para descompactar a lista
+-- Reconstrói a lista original a partir da versão compactada.
 descompactar_lista :: [(Int, Char)] -> [Char]
 descompactar_lista [] = []
 descompactar_lista ((n, c):xs) = replicate n c ++ descompactar_lista xs
